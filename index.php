172c6b42a49efff91f84a86091e46cc94fa132f0
Mike Owens <mocoding86@gmail.com>
2021-05-30 12:21:25 -0700

    work on overlay bg

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 
 <section class="hero">
     <div class="width-limiter">
-        <div class="overlay"></div>
+        <div class="overlay">
         <video autoplay muted loop id="bgVideo">
             <source src="../horizon/assets/horizon_trailer_bg.mp4" type="video/mp4">
             Your browser does not support HTML5 video.
         </video>
+        </div>
     </div>
 </section>
 
