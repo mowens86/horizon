5d9e0c48746477ac257dd9d3648e172492f24304
mocoding86 <mocoding86@gmail.com>
2021-07-05 20:41:06 -0700

    adjust cards and add newsletter sign-up section

--- index.php
+++ index.php
@@ -42,11 +42,11 @@
                     <li class="light-pfont mleft">Basic Hunter Gear</li>
                     <li class="light-pfont mleft">Poster</li>
                 </ul>
-                <div class="btn-container">
-                    <a href="./">
-                        <span class="card-btn">Pre-Order Basic</span>
-                    </a>
-                </div>
+            </div>
+            <div class="btn-container">
+                <a href="./">
+                    <span class="card-btn">Pre-Order Basic</span>
+                </a>
             </div>
         </div>
         <div class="card two">
@@ -60,12 +60,13 @@
                     <li class="light-pfont mleft">Poster</li>
                     <li class="light-pfont mleft">+2 Skill Points</li>
                 </ul>
-                <div class="btn-container">
+
+            </div>
+            <div class="btn-container">
                     <a href="./">
                         <span class="card-btn">Pre-Order Hunter</span>
                     </a>
                 </div>
-            </div>
         </div>
         <div class="card three">
             <div class="card-content">
@@ -79,11 +80,11 @@
                     <li class="light-pfont mleft">+4 Skill Points</li>
                     <li class="light-pfont mleft">Mammoth Override</li>
                 </ul>
-                <div class="btn-container">
-                    <a href="./">
-                        <span class="card-btn">Pre-Order Ultimate</span>
-                    </a>
-                </div>
+            </div>
+            <div class="btn-container">
+                <a href="./">
+                    <span class="card-btn">Pre-Order Ultimate</span>
+                </a>
             </div>
         </div>
     </div>
@@ -92,13 +93,21 @@
 <section class="gallery bg-img-utils section-padding overview-overlay">
     <div class="width-limiter overview-flex">
         <div class="overview-flex-left overview-content">
-            <h1 class="light-hfont">Game Overview</h1>
-            <p class="light-pfont">Join Aloy as she braves the Forbidden West – a majestic but dangerous frontier that conceals mysterious new threats.</p>
+            <h1 class="light-hfont">Sign-Up for the Newsletter</h1>
+            <form action="/action_page.php" method="POST">
+                <div class="form-label">
+                    <label class="form-label light-pfont" for="fname">Email</label><br>
+                </div>
+                
+                <input class="form-input" type="email" id="email" placeholder="Type your email"><br><br>
+                <div class="">
+                    <input class="card-btn" type="submit" value="Submit">
+                </div>
+            </form>
         </div>
         <div class="overview-flex-right overview-content">
-            <p class="light-pfont">Explore distant lands, fight bigger and more awe-inspiring machines, and encounter astonishing new tribes as you return to the far-future, post-apocalyptic world of Horizon.</p><br>
-            <p class="light-pfont">The land is dying. Vicious storms and an unstoppable blight ravage the scattered remnants of humanity, while fearsome new machines prowl their borders. Life on Earth is hurtling towards another extinction, and no one knows why.</p><br>
-            <p class="light-pfont">It's up to Aloy to uncover the secrets behind these threats and restore order and balance to the world. Along the way, she must reunite with old friends, forge alliances with warring new factions and unravel the legacy of the ancient past – all the while trying to stay one step ahead of a seemingly undefeatable new enemy.</p>
+            <p class="light-pfont">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolore eum incidunt reprehenderit quasi obcaecati ullam nam saepe, ipsum nostrum inventore officiis deleniti, assumenda optio rem fugit ducimus. Harum, consequuntur?</p><br>
+            <p class="light-pfont">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolore eum incidunt reprehenderit quasi obcaecati ullam nam saepe, ipsum nostrum inventore officiis deleniti, assumenda optio rem fugit ducimus. Harum, consequuntur?</p><br>
         </div>
     </div>
 </section>
